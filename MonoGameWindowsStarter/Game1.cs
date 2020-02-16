b55e7628536e6fe13b16119bba2680825ad3f118
connor hawley <rcohawley@yahoo.com>
2020-02-15 18:16:22 -0600

    test

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -18,6 +18,7 @@ namespace MonoGameWindowsStarter
             Content.RootDirectory = "Content";
         }
 
+
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
